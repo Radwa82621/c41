6d8fb14ad93cb55a99cefe3652c7f86ebf474079
radwa <radwa.taha82621@gmail.com>
2023-09-07 21:51:09 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { navbar } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TestComponent } from './components/test./test..component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navbar,
+    HomeComponent,
+    FooterComponent,
+    GalleryComponent,
+    TestComponent,
+    AboutComponent,
+    
   ],
   imports: [
     BrowserModule,
